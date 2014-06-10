e830a359d65da1cd4c2dc52b8966df609e4f2b62
Stefan <stadolf@gmail.com>
2014-06-10 02:43:04 +0200

    mongo urls Procfile

--- mongo.js
+++ mongo.js
@@ -2,6 +2,7 @@ var mongo = require('mongoskin');
 var Server = mongo.Server;
 var Db = mongo.Db;
 
-var db = mongo.db(process.env.MONGODB_URL, {native_parser:true});
+var mngUrl = process.env.MONGOSOUP_URL || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.MONGODB_URL;
+var db = mongo.db(mngUrl, {native_parser:true});
 
 module.exports = db;
